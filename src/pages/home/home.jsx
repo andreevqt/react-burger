93bab1b992280dbad6c4023b1693f4b34c8cb264
qprquo <qprquo@gmail.com>
2022-01-25 00:02:48 +0300

    Remove getItems call from home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,22 +1,16 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import BurgerIngredients from '../../components/burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../../components/burger-constructor/burger-constructor';
 import Col from '../../components/grid/col/col';
 import Row from '../../components/grid/row/row';
-import { getItems } from '../../services/actions/ingredients';
 import Base from '../../components/layout/base/base';
 import WithLoader from '../../components/with-loader/with-loader';
 
 const Home = () => {
-  const dispatch = useDispatch();
   const isLoading = useSelector((store) => store.order.isLoading);
-
-  useEffect(() => {
-    dispatch(getItems());
-  }, [dispatch]);
 
   return (
     <WithLoader isLoading={isLoading}>
