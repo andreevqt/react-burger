47b7838c344eafa1152861f7f97494a87b5457e5
qprquo <qprquo@gmail.com>
2022-02-13 05:45:57 +0300

    fix args type

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,8 +1,8 @@
 // https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_throttle
 
-const throttle = (cb: (...args: any) => void, time: number) => {
+const throttle = (cb: (...args: any[]) => void, time: number) => {
   let last = 0;
-  return (...args : any) => {
+  return (...args : any[]) => {
     const now = Date.now();
     if (now - last >= time) {
       cb(...args);
